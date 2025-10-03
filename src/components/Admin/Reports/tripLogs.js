b5d2a776b269d3e4feb3c1237610093d851bc634
Kelvin Reyes <kelvinandreiareyes@gmail.com>
2025-10-04 01:25:01 +0800

    UPDATED FINAL V2

--- src/components/Admin/Reports/tripLogs.js
+++ src/components/Admin/Reports/tripLogs.js
@@ -788,7 +788,7 @@ const TripLogs = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
+                    d="M16 15v-1a4 4 0 00-4-4H8m0 0l3 3m-3-3l3-3m9 14V5a2 2 0 00-2-2H6a2 2 0 00-2 2v16l4-2 4 2 4-2 4 2z"
                   />
                 </svg>
               </div>
